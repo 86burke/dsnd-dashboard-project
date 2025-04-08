ac80eef85404c36b3deeff6b471b6236899bc6c3
86burke <86burke@gmail.com>
2025-04-08 18:42:33 -0500

    query database

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,27 +1,27 @@
 # Import any dependencies needed to execute sql queries
-# YOUR CODE HERE
+from .sql_execution import *
 
 # Define a class called QueryBase
 # Use inheritance to add methods
 # for querying the employee_events database.
-# YOUR CODE HERE
+Class QueryBase:
 
     # Create a class attribute called `name`
     # set the attribute to an empty string
-    # YOUR CODE HERE
+    name = ""
 
     # Define a `names` method that receives
     # no passed arguments
-    # YOUR CODE HERE
+   def names(self)
         
         # Return an empty list
-        # YOUR CODE HERE
+        return []
 
 
     # Define an `event_counts` method
     # that receives an `id` argument
     # This method should return a pandas dataframe
-    # YOUR CODE HERE
+    def event_counts(self, id):
 
         # QUERY 1
         # Write an SQL query that groups by `event_date`
@@ -31,13 +31,21 @@
         # Use f-string formatting to set the name
         # of id columns used for joining
         # order by the event_date column
-        # YOUR CODE HERE
+        sql_query = f''
+            select "event_date"
+                ,sum("positive_events") as 'Positive Events'
+                ,sum("negative_events") as 'Negative Events'
+         from "{self.name}"
+         left join "employee_events" using ("{self.name}_id")
+         where "{self.name}"."{self.name}_id" = {id}
+         group by "event_date"
+         order by "event_date";
             
-    
+        return *.pandas_query(self, sql_query)
 
     # Define a `notes` method that receives an id argument
     # This function should return a pandas dataframe
-    # YOUR CODE HERE
+    def notes(self, id):
 
         # QUERY 2
         # Write an SQL query that returns `note_date`, and `note`
@@ -46,5 +54,12 @@
         # with f-string formatting
         # so the query returns the notes
         # for the table name in the `name` class attribute
-        # YOUR CODE HERE
+        sql_query = f''
+            select "note_date"
+            ,note
+            from "notes"
+            left join "{self.name}" using ("{self.name}_id")
+            where "{self.name}"."{self.name}_id" = {id}
+            
+        return *.pandas_query(self, sql_query)
 
