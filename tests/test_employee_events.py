8d575a1d8fefdd459a7276825fa56f3bcdad4ae5
86burke <86burke@gmail.com>
2025-04-17 17:46:40 -0500

    pytesting

--- tests/test_employee_events.py
+++ tests/test_employee_events.py
@@ -17,7 +17,7 @@ def db_path():
 
     # Using the `project_root` variable
     # return a pathlib object for the `employee_events.db` file
-    return project_root/"python_package"/"employee_events"/"employee_events.db"
+    return project_root / 'employee_events.db'
 
 # Define a function called
 # `test_db_exists`
@@ -32,7 +32,7 @@ def test_db_exists(db_path):
     # using the pathlib `.is_file` method
     # assert that the sqlite database file exists
     # at the location passed to the test_db_exists function
-    assert db_path.is_file(), f"Database file does not exist at {db_path}"
+    assert db_path.is_file()
 
 
 @pytest.fixture
@@ -43,8 +43,7 @@ def db_conn(db_path):
 
 @pytest.fixture
 def table_names(db_conn):
-    name_tuples = db_conn.execute(
-        "SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+    name_tuples = db_conn.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
     return [x[0] for x in name_tuples]
 
 # Define a test function called
@@ -58,7 +57,7 @@ def test_employee_table_exists(table_names):
     # Assert that the string 'employee'
     # is in the table_names list # noqa: E501
 
-    assert "employee" in table_names
+    assert 'employee' in table_names, "'employee' table does not exist in the database"
 
 # Define a test function called
 # `test_team_table_exists`
@@ -71,7 +70,7 @@ def test_team_tabel_exists(table_names):
     # Assert that the string 'team'
     # is in the table_names list # noqa: E501
 
-    assert "team" in table_names
+    assert 'team' in table_names, "'team' table does not exist in the database"
 
 # Define a test function called
 # `test_employee_events_table_exists`
@@ -84,4 +83,4 @@ def test_employee_events_table_exists(table_names):
     # Assert that the string 'employee_events'
     # is in the table_names list
 
-    assert 'employee_events' in table_names
+    assert 'employee_events' in table_names, "'employee_events' table does not exist in the database"
